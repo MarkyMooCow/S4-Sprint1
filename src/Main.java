9db0b99a23904f1b1a4804ac67c2f5bd7eee7c1e
MarkyMooCow <7105markhunter@gmail.com>
2022-10-28 20:54:51 -0230

    we getting somewhere

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
 public class Main {
     // Need to figure out how arrays work for this sort of thing...
     public static void main(String[] args) {
-            Person Person1 = new Person(
-                    "Air Ray","2 Detroit Street",
-                    "Detroitborn@dmail.com", "444-4444",
-                    "Single", "11/10/11", "1 Year");
+        Person Person1 = new Person(
+                "Air Ray","2 Detroit Street",
+                "Detroitborn@dmail.com", "444-4444",
+                "Single", "11/10/11", "1 Year");
+        Person Person2 = new Person(
+                "Detroit Michigan","3 Detroit Street",
+                "Detroithimself@dmail.com", "420-6969",
+                "Premium", "11/11/11", "5 Years");
+        Person Person3 = new Person(
+                "Bob","0 Bob Bob",
+                "Bob@bob.bob", "808-8008",
+                "Bob", "08/08/08", "10 Years");
 
         Tourney Tourney1 = new Tourney(
                 "11/11/11", "11/15/11", "Detroit, Michigan",
-                null, 3, 5.00, 10.00);
+                3, 5.00, 10.00);
 
         Tourney1.addCompetitor(-1, Person1);
         Tourney1.addCompetitor(4, Person1);
         Tourney1.addCompetitor(0, Person1);
+        Tourney1.addCompetitor(1, Person2);
+        Tourney1.addCompetitor(2, Person3);
         Tourney1.getCompetitors();
-    }
-}
+        System.out.println(Tourney1.showFinalStandings());
+        System.out.println(Tourney1.createFinalStandings(0, 1, 2));
+        System.out.println(Tourney1.showFinalStandings());
+
+}}
--- src/Tourney.java
+++ src/Tourney.java
@@ -1,6 +1,6 @@
 public class Tourney {
-        public String startDate, endDate, location, finalStandings;
-        public String[] competitors;
+        public String startDate, endDate, location;
+        public String[] competitors, finalStandings;
         public double entryFee, cashPrize;
 
         public Tourney() // Default Constructor
@@ -9,20 +9,20 @@ public class Tourney {
             this.endDate = null;
             this.location = null;
             this.competitors = new String[5]; // 5 will be the default competitor amount;
-            this.finalStandings = null;
+            this.finalStandings = new String[3];
             this.entryFee = 0;
             this.cashPrize = 0;
         }
 
     public Tourney(
-            String startDate, String endDate, String location, String finalStandings,
+            String startDate, String endDate, String location,
             int compLength, double entryFee, double cashPrize)
         {
             this.startDate = startDate;
             this.endDate = endDate;
             this.location = location;
             this.competitors = new String[compLength];
-            this.finalStandings = finalStandings;
+            this.finalStandings = new String[3];
             this.entryFee = entryFee;
             this.cashPrize = cashPrize;
         }
@@ -49,8 +49,32 @@ public class Tourney {
             if (i > this.competitors.length || i < 0) {
                 System.out.println("I is out of the range in this scenario, so don't execute the Queen Of France");
             } else {
-                System.out.println("I is in the range, so BRAAAAP");
-            }}
+                if (this.competitors[i] != null) {
+                    System.out.println("Slot filled! " + this.competitors[i] + " will be replaced!");
+                    this.competitors[i] = Peep.getName();
+                    System.out.println("Competitor replaced!");
+                } else {
+                    this.competitors[i] = Peep.getName();
+                    System.out.println("Competitor added!");
+                }}}
+    public String createFinalStandings(int first, int second, int third) {
+        this.finalStandings[0] = this.competitors[first];
+        this.finalStandings[1] = this.competitors[second];
+        this.finalStandings[2] = this.competitors[third];
+        return "Final Standings Created!" +
+                "\n First: " + this.finalStandings[0] +
+                "\n Second: " + this.finalStandings[1] +
+                "\n Third: " + this.finalStandings[2];
+        }
+        public String showFinalStandings(){
+            if (this.finalStandings[0] == null) {
+                return "There is no final standings yet! You must create them!";
+            } else {
+            return "Final Standings!" +
+                "\n First: " + this.finalStandings[0] +
+                "\n Second: " + this.finalStandings[1] +
+                "\n Third: " + this.finalStandings[2];
+        }}
     /* public String getFinalStandings(){
         return this.finalStandings;
     } */
